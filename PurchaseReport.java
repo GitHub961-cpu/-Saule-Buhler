c9010716141afbe6d9c85eb01c39a94950984744
GitHub961-cpu <68203173+GitHub961-cpu@users.noreply.github.com>
2020-07-12 15:08:56 -0600

    Add files via upload

---
+++ PurchaseReport.java
@@ -0,0 +1,25 @@
+//Saule Buhler
+
+public class PurchaseReport 
+{
+	public static void main(String[] args) 
+	{
+		String item1 = "Shirt";
+		String item2 = "Pants";
+		String item3 = "Shoes";
+		String item4 = "Belt";
+		String item5 = "Coat";
+		String total = "Total";
+		
+		System.out.println("=======================");
+		System.out.println("Purchase Report");
+		System.out.println("=======================");
+		System.out.printf("%s \t\t $%,.2f\n", item1,21.99);
+		System.out.printf("%s \t\t $%,.2f\n", item2,49.97);
+		System.out.printf("%s \t\t $%,.2f\n", item3,89.50);
+		System.out.printf("%s \t\t $%,.2f\n", item4,19.99);
+		System.out.printf("%s \t        $%,.2f\n", item5,129.95);
+		System.out.println("=======================");
+		System.out.printf("%s \t        $%,.2f\n", total,311.40);
+	}
+}
